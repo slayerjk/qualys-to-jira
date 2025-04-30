08c41cd295ba91380ab233c7aecca0cc03bfefbe
slayerjk <slayer.jk@mail.ru>
2025-04-30 16:13:09 +0500

    list_qualys_last_processed_reports: failback login added

--- qualys-to-jira.py
+++ qualys-to-jira.py
@@ -182,7 +182,7 @@ try:
         except ValueError as e:
             logging.exception('FAILED: forming last processed report list, not number in list, exiting...')
             send_mail_report(*mail_settings, mail_type='error')
-            exit()
+            list_qualys_last_processed_reports = []
     logging.info('DONE: forming Qualys last processed report list')
     logging.info(f'Last processed reports is:\n{list_qualys_last_processed_reports}\n')        
 
@@ -198,12 +198,13 @@ try:
             
             logging.info(f'Checking if Report id({rep_id}) has been processed already')
             is_processed = False
-            for val in list_qualys_last_processed_reports:
-                if int(rep_id) <= int(val):
-                    logging.info(f'{rep_id} HAS BEEN PROCESSED ALREADY, skipping this report\n')
-                    user_report_temp.write(f'{rep_id}:{rep_title} - HAS BEEN PROCESSED ALREADY\n\n')
-                    is_processed = True
-                    break
+            if len(list_qualys_last_processed_reports) != 0:
+                for val in list_qualys_last_processed_reports:
+                    if int(rep_id) <= int(val):
+                        logging.info(f'{rep_id} HAS BEEN PROCESSED ALREADY, skipping this report\n')
+                        user_report_temp.write(f'{rep_id}:{rep_title} - HAS BEEN PROCESSED ALREADY\n\n')
+                        is_processed = True
+                        break
             if not is_processed:
                 logging.info(f'NEW REPORT FOR JIRA HAS FOUND: {rep_id}:{rep_title}\n')
                 user_report_temp.write(f'{rep_id}:{rep_title} - NEW REPORT TO PROCESS\n\n')
